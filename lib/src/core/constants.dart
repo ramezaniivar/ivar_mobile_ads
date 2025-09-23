e012a5a9fd3a4b9b739e466e6b02287f2fd8ef5a
Ramezani <ramezaniivar0975@gmail.com>
2025-09-23 12:41:26 +0330

    new version 0.3.0

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 final class Constants {
-  // static const baseUrl = 'https://ivar-ads.com';
-  static const baseUrl = 'http://172.28.187.193:8080';
+  static const baseUrl = 'https://ivar-ads.com';
   static const apiV1 = '$baseUrl/api/v1';
   static const apiV2 = '$baseUrl/api/v2';
 
